0d8a6d723d904bddfdf6fa3f20759c7804ecfe78
Sorabh001 <158773135+Sorabh001@users.noreply.github.com>
2024-03-16 11:50:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+    document.getElementById("aadhya-output").addEventListener("click", function() {
+                this.select();
+                document.execCommand("copy");
+                alert("Text copied to clipboard!");
+        });
+        window.onload = function() {
+    var logoText = document.getElementById("logoText");
+    var logoImage = document.getElementById("logoImage");
+    setInterval(function() {
+      if (logoText.style.display === "none") {
+        logoText.style.display = "block";
+        logoImage.style.display = "none";
+      } else {
+        logoText.style.display = "none";
+        logoImage.style.display = "block";
+      }
+    }, 3000);
+  };
